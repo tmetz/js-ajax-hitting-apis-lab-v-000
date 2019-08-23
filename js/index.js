a7c43323d8eb6bb80b52df35bf368ccd79487c52
Tammy Metz <tammy@tammymetz.com>
2019-08-23 03:13:56 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ function displayRepositories() {
 function getCommits(el) {
   let username = document.getElementById('username').value;
   const name = el.dataset.repo;
-  const req = new HXMLHttpRequest();
+  const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits());
   req.open('GET', `https://api.github.com/repos/#{username}/#{name}/commits`);
   req.send();
